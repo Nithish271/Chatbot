56db709fed8352df575587d1a212600a150f3565
Nithish.P <93858556+Nithish271@users.noreply.github.com>
2022-04-30 18:36:09 +0530

    first commit

---
+++ web (1).py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS
+from chatbot import response
+
+app=Flask(__name__)
+CORS(app)
+
+@app.post("/predict")
+def predict():
+    text = request.get_json().get("message")
+    # T0D0: check if text is valid 
+    res = response(text)
+    message ={"answer": res}
+    return jsonify(message)
+
+if __name__=="__main__":
+    app.run(debug=True)
